d2edc32182722c507f471722947d56067a0fc2a5
Leonardo <lorlando@est.frba.utn.edu.ar>
2020-12-27 22:04:15 -0300

    exercise 4

--- exercise4-howSum.js
+++ exercise4-howSum.js
@@ -7,3 +7,27 @@ If there are multiple combinations possible, you may return any single one.
 
 */
 
+const howSum = (targetSum, numbers, memo = {}) => {
+    if (targetSum in memo) return memo[targetSum];
+    if (targetSum === 0) return [];
+    if (targetSum < 0) return null;
+
+    for (let num of numbers) {
+        const remainder = targetSum - num;
+        const remainderResult = howSum(remainder, numbers);
+        if (remainderResult !== null){
+            memo[targetSum] = [...remainderResult, num];
+            return memo[targetSum];
+        }
+    }
+    memo[targetSum] = null
+    return null;
+}
+
+//complexity brute force
+//O(n^m * m) Time
+//O(m) space
+
+//complexity memoized
+//O(n*m*m) Time
+//O(m*m) space
