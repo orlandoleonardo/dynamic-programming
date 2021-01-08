a86688e2cb28707e5f353aaf46a987f315128fcb
Leonardo <lorlando@est.frba.utn.edu.ar>
2021-01-08 19:34:13 -0300

    agrego enunciado de ejercicio 7

--- exercise7-countConstruct.js
+++ exercise7-countConstruct.js
@@ -1,3 +1,12 @@
+/*
+Write a function 'countConstruct(target, wordBank)' that accepts a target string and an array of strings.
+
+The function should return the number of ways that the 'target' can be constructed by concatenating elements of the 'wordBank' array.
+
+You may reuse elements of 'wordBank' as many times as needed.
+
+ */
+
 const counConstruct = (target, wordBank, memo = {}) => {
     if (target === '') return 1;
     if (target in memo) return memo[target];
